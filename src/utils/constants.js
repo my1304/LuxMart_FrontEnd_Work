3a8e9cae67656fb841c2eaed992d95122d0534f5
Vyacheslav <my1304@t-online.de>
2025-02-27 21:03:45 +0100

    railway

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,9 +2,8 @@
 //export const BASE_URL = "http://10.1.1.19:8088/api";
 //export const BASE_URL = "https://luxmart_cpp_backend-production.up.railway.app/api";
 //export const BASE_URL = "https://luxmart-cpp-backend.onrender.com/api";
-//export const BASE_URL = "https://luxmartcppbackend-production.up.railway.app/api";
-export const BASE_URL = "https://luxmart-cpp-backend-qqd7.onrender.com/api";
-
+export const BASE_URL = "https://luxmartcppbackend-production.up.railway.app/api";
+//export const BASE_URL = "https://luxmart-cpp-backend-qqd7.onrender.com/api";
 
 export const Category_URL = "http://localhost/uploads/images/categories/";
 export const Product_URL = "http://localhost/uploads/images/categories/";
